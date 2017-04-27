23952645d1b3b8a88480ffb1361215e91ecb0956
Brent Perry <burentoperii@gmail.com>
2017-04-27 14:18:23 -0500

    add prompt for bank balance

--- phoneStore.js
+++ phoneStore.js
@@ -1,13 +1,14 @@
-var balance = 300;
+var balance = prompt("What is your current bank balance?");
 var phones = 0;
 var totalBill = 0;
 const ACCESSORY_PRICE = 9.99;
 const PHONE_PRICE = 99.99;
 const TAX_RATE = 0.0625;
+const PRICE_THRESHHOLD = 110;
 
 function getTotalPrice() {
 	var total = PHONE_PRICE;
-	if (PHONE_PRICE + ACCESSORY_PRICE < 110) {
+	if (PHONE_PRICE + ACCESSORY_PRICE < PRICE_THRESHHOLD) {
 		total += ACCESSORY_PRICE;
 	}
 	return total * (1 + TAX_RATE);
