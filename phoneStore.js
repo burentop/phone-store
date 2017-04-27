6009c26e99c0f5ff69da99611d163c87a00b9abe
Brent Perry <burentoperii@gmail.com>
2017-04-27 14:12:55 -0500

    add tax rate and total bill

--- phoneStore.js
+++ phoneStore.js
@@ -1,19 +1,22 @@
 var balance = 300;
 var phones = 0;
+var totalBill = 0;
 const ACCESSORY_PRICE = 9.99;
 const PHONE_PRICE = 99.99;
+const TAX_RATE = 0.0625;
 
 function getTotalPrice() {
 	var total = PHONE_PRICE;
 	if (PHONE_PRICE + ACCESSORY_PRICE < 110) {
 		total += ACCESSORY_PRICE;
 	}
-	return total;
+	return total * (1 + TAX_RATE);
 }
 
 function buyPhone() {
 	balance -= getTotalPrice();
 	phones++;
+	totalBill += getTotalPrice();
 }
 
 while (balance >= getTotalPrice()) {
@@ -21,3 +24,4 @@ while (balance >= getTotalPrice()) {
 }
 
 console.log("Phones bought: " + phones);
+console.log("Total: $" + totalBill.toFixed(2));
